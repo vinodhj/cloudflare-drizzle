d23e7deb3c00987dd18d9e9f0771f4aa4c1e6717
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-26 12:26:29 +0530

    created a rest url for insert into db

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,32 @@ export interface Env {
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
-    const db = dbConnection(env.TURSO_URL, env.TURSO_AUTH_TOKEN);
-    const nano_int_id = customAlphabet('1234567890', 6);
+    const url = new URL(request.url);
+    const path = url.pathname;
 
-    const insert_values = {
-      id: Number(nano_int_id()),
-      fullName: nanoid() as string,
-      phone: nano_int_id(10) as string,
-    };
-    const response = await db.insert(users).values(insert_values).returning().get();
-    console.log(`Inserted successfully:`, JSON.stringify(response));
-    return new Response('Hello World!');
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      });
+    }
+
+    if (path === '/dizzle-test') {
+      const db = dbConnection(env.TURSO_URL, env.TURSO_AUTH_TOKEN);
+      const nano_int_id = customAlphabet('1234567890', 6);
+
+      const insert_values = {
+        id: Number(nano_int_id()),
+        fullName: nanoid() as string,
+        phone: nano_int_id(10) as string,
+      };
+      const response = await db.insert(users).values(insert_values).returning().get();
+      return new Response(JSON.stringify({ msg: 'Inserted successfully', response }));
+    }
+
+    return new Response('Not found', { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
